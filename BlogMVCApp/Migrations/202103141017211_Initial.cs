7937d278568716abfeb6ff0802a1b93939bfd4bd
Ali <U_orucova@hotmail.com>
2021-03-14 20:12:43 +0400

    Commit

---
+++ BlogMVCApp/Migrations/202103141017211_Initial.cs
@@ -0,0 +1,141 @@
+﻿namespace BlogMVCApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Articles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 40),
+                        AuthorId = c.Int(nullable: false),
+                        PublishDate = c.DateTime(nullable: false),
+                        WrittenDate = c.DateTime(nullable: false),
+                        ImagePath = c.String(nullable: false),
+                        ShortDestcription = c.String(nullable: false),
+                        MenuId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Authors", t => t.AuthorId, cascadeDelete: true)
+                .ForeignKey("dbo.Menus", t => t.MenuId, cascadeDelete: true)
+                .Index(t => t.AuthorId)
+                .Index(t => t.MenuId);
+            
+            CreateTable(
+                "dbo.Authors",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Username = c.String(nullable: false, maxLength: 40),
+                        Password = c.String(nullable: false, maxLength: 30),
+                        IsActive = c.Boolean(nullable: false),
+                        Email = c.String(nullable: false),
+                        IsAuthor = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Article_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Articles", t => t.Article_Id)
+                .Index(t => t.Article_Id);
+            
+            CreateTable(
+                "dbo.Comments",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Text = c.String(nullable: false, maxLength: 100),
+                        UserId = c.Int(nullable: false),
+                        ArticleId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Articles", t => t.ArticleId, cascadeDelete: true)
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: false)
+                .Index(t => t.UserId)
+                .Index(t => t.ArticleId);
+            
+            CreateTable(
+                "dbo.Menus",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Text = c.String(nullable: false, maxLength: 50),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Tags",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.TagArticles",
+                c => new
+                    {
+                        Tag_Id = c.Int(nullable: false),
+                        Article_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Tag_Id, t.Article_Id })
+                .ForeignKey("dbo.Tags", t => t.Tag_Id, cascadeDelete: true)
+                .ForeignKey("dbo.Articles", t => t.Article_Id, cascadeDelete: true)
+                .Index(t => t.Tag_Id)
+                .Index(t => t.Article_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.TagArticles", "Article_Id", "dbo.Articles");
+            DropForeignKey("dbo.TagArticles", "Tag_Id", "dbo.Tags");
+            DropForeignKey("dbo.Articles", "MenuId", "dbo.Menus");
+            DropForeignKey("dbo.Comments", "UserId", "dbo.Users");
+            DropForeignKey("dbo.Comments", "ArticleId", "dbo.Articles");
+            DropForeignKey("dbo.Categories", "Article_Id", "dbo.Articles");
+            DropForeignKey("dbo.Authors", "UserId", "dbo.Users");
+            DropForeignKey("dbo.Articles", "AuthorId", "dbo.Authors");
+            DropIndex("dbo.TagArticles", new[] { "Article_Id" });
+            DropIndex("dbo.TagArticles", new[] { "Tag_Id" });
+            DropIndex("dbo.Comments", new[] { "ArticleId" });
+            DropIndex("dbo.Comments", new[] { "UserId" });
+            DropIndex("dbo.Categories", new[] { "Article_Id" });
+            DropIndex("dbo.Authors", new[] { "UserId" });
+            DropIndex("dbo.Articles", new[] { "MenuId" });
+            DropIndex("dbo.Articles", new[] { "AuthorId" });
+            DropTable("dbo.TagArticles");
+            DropTable("dbo.Tags");
+            DropTable("dbo.Menus");
+            DropTable("dbo.Comments");
+            DropTable("dbo.Categories");
+            DropTable("dbo.Users");
+            DropTable("dbo.Authors");
+            DropTable("dbo.Articles");
+        }
+    }
+}
