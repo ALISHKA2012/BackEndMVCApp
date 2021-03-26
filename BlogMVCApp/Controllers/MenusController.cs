7794e9ad1411a41e5c8f9967b5995387c866100c
Ali <U_orucova@hotmail.com>
2021-03-26 23:45:18 +0400

    menu update commit

---
+++ BlogMVCApp/Controllers/MenusController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlogMVCApp.Data;
+
+namespace BlogMVCApp.Controllers
+{
+    public class MenusController : Controller
+    {
+        private readonly BlogContext _blogContext;
+        public MenusController()
+        {
+            _blogContext = new BlogContext();
+        }
+        public PartialViewResult AllMenus()
+        {
+            var menus = _blogContext.Menus.Where( m => m.IsActive == true).ToList();
+            return PartialView(menus);
+        }
+    }
+}
